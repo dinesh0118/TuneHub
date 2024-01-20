8c8f6ae27f3ce7d420731f24b9e768004b9c47b2
EOVO <EOVO@DineshReddy>
2024-01-20 19:25:52 +0530

    1st version of tune hub

---
+++ src/main/java/com/tunehub/controller/PlaylistController.java
@@ -0,0 +1,58 @@
+package com.tunehub.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.tunehub.entities.Playlist;
+import com.tunehub.entities.Song;
+import com.tunehub.services.PlaylistService;
+import com.tunehub.services.SongService;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+
+@Controller
+public class PlaylistController {
+	@Autowired
+	SongService songService;
+	
+	@Autowired
+	PlaylistService playlistService;
+	@GetMapping("/createPlaylist")
+	public String createPlayList(Model model) {
+		
+		List<Song> songList = songService.fetchAllSongs();
+		model.addAttribute("songs", songList);
+		return "createPlayList";
+	}
+	@PostMapping("/addPlaylist")
+	public String addPlaylist(@ModelAttribute Playlist playlist) {
+		//updating playlist table
+		playlistService.addPlaylist(playlist);
+		
+		//updating song table
+		List<Song> songList = playlist.getSongs();
+		for(Song s : songList) {
+			s.getPlaylists().add(playlist);
+			//update song object in db
+			songService.updateSong(s);
+		}
+		return "adminHome";
+	}
+	@GetMapping("/viewPlaylists")
+	public String viewPlaylists(Model model) {
+		
+		List<Playlist> allPlaylists =  playlistService.fetchAllPlaylists();
+		model.addAttribute("allPlaylists", allPlaylists);
+		return "displayPlaylists";
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/tunehub/controller/UsersController.java
@@ -0,0 +1,68 @@
+package com.tunehub.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.tunehub.entities.Users;
+import com.tunehub.services.UsersService;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class UsersController {
+
+	@Autowired
+	UsersService service;
+
+	@PostMapping("/register")
+	public String addUsers(@ModelAttribute Users user) {
+		boolean userStatus = service.emailExists(user.getEmail());
+		if (userStatus == false) {
+			service.addUser(user);
+			System.out.println("User Added");
+		} else {
+			System.out.println("User is already exsist");
+		}
+		return "home";
+	}
+
+	@PostMapping("/validate")
+	public String validate(@RequestParam("email") String email,
+			@RequestParam("password") String password,
+			HttpSession session) {
+		
+		if (service.validateUser(email, password) == true) {
+			String role = service.getRole(email);
+			
+			session.setAttribute("email", email);
+			
+			if(role.equals("admin")) {
+				return "adminHome";
+			}else {
+				return "customerHome";
+			}
+		} else {
+			return "login";
+		}
+	}
+	
+	
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		
+		session.invalidate();
+		return "login";
+	}
+	
+	
+	
+	
+}
